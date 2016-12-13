5438ffd4d02dc41fa2ac7464a27f4d9ad5271a09
sugarshin <shinsugar@gmail.com>
2016-12-13 21:56:15 +0900

    Add Button

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@
 import decorateComponentWithProps from 'decorate-component-with-props'
 import createBlockRendererFn from './createBlockRendererFn'
 import createBlockRenderMap from './createBlockRenderMap'
-import CheckableListItem from './CheckableListItem'
 import createOnTab from './createOnTab'
 import blockStyleFn from './blockStyleFn'
+import CheckableListItem from './CheckableListItem'
+import Button from './Button'
 
 const checkableListPlugin = (config: Object = {}): Object => {
+  const store = {
+    getEditorState: null,
+    setEditorState: null,
+  }
   const theme = config.theme ? config.theme : {}
   const disabled = !!config.disabled
   const checkableListItemProps = {
@@ -28,10 +33,20 @@ const checkableListPlugin = (config: Object = {}): Object => {
     maxDepth: config.maxDepth || 4,
   }
 
+  const buttonConfig = {
+    store,
+    theme: config.buttonTheme || {},
+  }
+
   return {
+    initialize({ setEditorState, getEditorState }) {
+      store.setEditorState = setEditorState
+      store.getEditorState = getEditorState
+    },
     blockRendererFn: createBlockRendererFn(blockRendererConfig),
     blockRenderMap: createBlockRenderMap(blockRenderMapConfig),
     onTab: createOnTab(onTabConfig),
+    Button: decorateComponentWithProps(Button, buttonConfig),
     blockStyleFn,
   }
 }
